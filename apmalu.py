23028be1e8feeded50dc6670dcc02523ac10956f
Tyler Reddy <tyler.je.reddy@gmail.com>
2015-11-28 16:03:06 +0000

    Library code has been adjusted to allow for production of preliminary results in testing, mostly with temporary workarounds to poorly-understood problems.

--- apmalu.py
+++ apmalu.py
@@ -1,7 +1,8 @@
 '''Calculate area per molecule in any lipid ultrastructure (APMALU).'''
 
 import numpy as np
-import scipy
+import scipy.spatial
+import cPickle as pickle #debug work
 
 class AreaMolecule(object):
     '''Calculate the area per molecule for any 
@@ -23,9 +24,25 @@ class AreaMolecule(object):
         vor_vertices = vor.vertices
         index = 0
         list_head_simplex_coords = [] #for the 'head' simplices I'll eventually want to calculate SA of
-        for voronoi_region in vor.regions:
+        for voronoi_region in vor.regions[:-1]: #for some reason len(vor.regions) = number of generators + 1 -- why?
+            print 'index:', index, 'of', self.centroids.shape[0]
+            #print 'voronoi_region:', voronoi_region
+            #print 'num vertices in voronoi region:', len(voronoi_region)
             voronoi_region_vertex_coords = vor_vertices[voronoi_region]
-            hull = scipy.spatial.ConvexHull(voronoi_region_vertex_coords)
+            #print 'voronoi_region_vertex_coords.shape:', voronoi_region_vertex_coords.shape
+            try:
+                hull = scipy.spatial.ConvexHull(voronoi_region_vertex_coords)
+            except: #try iteratively removing the points until ConvexHull works (need to understand this issue better)
+                for skip_value in xrange(1,8): #remove up to 7 points
+                    try:
+                        print 'trying skip_value:', skip_value
+                        print 'index:', index
+                        hull = scipy.spatial.ConvexHull(voronoi_region_vertex_coords[skip_value:,...])
+                    except: 
+                        if skip_value < 7:
+                            continue
+                        else:
+                            print 'failed to recover after reaching skip_value', skip_value, 'at index', index
             simplices = hull.points[hull.simplices]
             #find the centroid of each simplex of the polyhedron
             simplex_centroids = np.array([np.average(simplex, axis = 0) for simplex in simplices])
@@ -35,4 +52,5 @@ class AreaMolecule(object):
             index_closest_simplex = np.argmin(distance_matrix)
             simplex_of_interest = simplices[index_closest_simplex]
             list_head_simplex_coords.append(simplex_of_interest)
+            index += 1
         return list_head_simplex_coords
